6f2f30a7a4d4eb5327c656493d1becb5cbeebf12
Barış karakuş <bariscan9788@gmail.com>
2024-10-02 20:46:28 +0300

    edit

--- middleware/auth.go
+++ middleware/auth.go
@@ -1,55 +1,54 @@
 package middleware
 
 import (
-    "net/http"
-    "strings"
-	"os"
-	"fmt"
-    "github.com/golang-jwt/jwt/v4"
-    "github.com/labstack/echo/v4"
 	model "articles-api/models/token"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/labstack/echo/v4"
 )
 
-
 func AuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
-    return func(c echo.Context) error {
-        authHeader := c.Request().Header.Get("Authorization")
+	return func(c echo.Context) error {
+		authHeader := c.Request().Header.Get("Authorization")
 
-        if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
-            if c.Request().Method == "GET" {
-                return next(c)
-            }
-            return c.JSON(http.StatusUnauthorized, echo.Map{
-                "error": "Missing or invalid Authorization header",
-            })
-        }
-       
+		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+			if c.Request().Method == "GET" {
+				return next(c)
+			}
+			return c.JSON(http.StatusUnauthorized, echo.Map{
+				"error": "Missing or invalid Authorization header",
+			})
+		}
+
 		acces_token := strings.Split(authHeader, " ")[1]
-		
+
 		claims := &model.Claim{}
-		
+
 		secretKey := os.Getenv("JWT_SECRET")
-        
-        if _, err := jwt.ParseWithClaims(acces_token, claims, func(token *jwt.Token) (interface{}, error) {
+
+		if _, err := jwt.ParseWithClaims(acces_token, claims, func(token *jwt.Token) (interface{}, error) {
 			if _, ok := token.Method.(*jwt.SigningMethodHMAC); ok {
 				return []byte(secretKey), nil
 			}
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-		}) ; err != nil {
+		}); err != nil {
 			return c.JSON(http.StatusUnauthorized, echo.Map{
-				"error" : err.Error(),
+				"error": err.Error(),
 			})
 		}
-		
-        if err := claims.Valid() ; err != nil {
-            return c.JSON(http.StatusUnauthorized, echo.Map{
-                "error": "Invalid or expired token",
-            })
-        }
 
-		c.Set("user",claims)
-		
-    
-        return next(c)
-    }
+		if err := claims.Valid(); err != nil {
+			return c.JSON(http.StatusUnauthorized, echo.Map{
+				"error": "Invalid or expired token",
+			})
+		}
+
+		c.Set("user", claims)
+
+		return next(c)
+	}
 }
